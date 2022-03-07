752724e46cf4630820fcdb2ead8d2858c5546b12
Willy Jansen <willyjansen@protonmail.com>
2022-03-07 10:21:30 +0900

    disable dry_run

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ fn main() {
     let journal_path = var("JOURNAL_PATH").expect("JOURNAL_PATH is not yet set");
 
     let args: Vec<String> = args().collect();
+    if args.len() != 3 {
+        panic!("Invalid arguments!");
+    }
     let action: Action = args[1].parse().expect("Action is unknown");
     let date_type: DateType = args[2].parse().expect("Date type is unknown");
 
@@ -12,6 +15,6 @@ fn main() {
         home: journal_path,
         action: action,
         date_type: date_type,
-        dry_run: true
+        dry_run: false
     });
 }
